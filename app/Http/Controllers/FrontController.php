ed2fce6ff59eeb0fb8f0a73ba11a08b22ab648b1
Wes Dollar <wes@wesdollar.com>
2018-03-28 17:23:17 -0400

    initial commit

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+
+    public function index() {
+
+        $meta = [
+            'title' => 'Pediatric Associates of Savannah',
+            'description' => '',
+        ];
+
+        $offices = [
+            'main' => [
+                'id' => 'main',
+                'title' => 'Main',
+                'phone' => '912.355.2462',
+                'address' => '4600 Waters Ave, Ste 100',
+                'city' => 'Savannah',
+                'zip' => '31404',
+                'hours' => [
+                    '8am – 5pm Mon thru Fri',
+                    '8am – 11am Weekend/Holidays'
+                ],
+            ],
+            'pooler' => [
+                'id' => 'pooler',
+                'title' => 'Pooler',
+                'phone' => '912.330.5010',
+                'address' => '110  Medical Park Drive',
+                'city' => 'Pooler',
+                'zip' => '31322',
+                'hours' => [
+                    '8am – 5pm Mon thru Fri',
+                ],
+            ],
+            'whitemarsh' => [
+                'id' => 'whitemarsh',
+                'title' => 'Whitemarsh Island',
+                'phone' => '912.330.5010',
+                'address' => '1001  Memorial Lane',
+                'city' => 'Whitemarsh',
+                'zip' => '31404',
+                'hours' => [
+                    '8am – 5pm Mon thru Fri',
+                ],
+            ],
+        ];
+
+        $doctors = [
+            [
+                'name' => 'Dr. Michael D. DeMauro',
+                'credentials' => 'MD, FAAP',
+                'img' => 'dr-michael-demauro',
+            ],
+            [
+                'name' => 'Dr. Diane R. Savage-Pedigo',
+                'credentials' => 'MD, FAAP',
+                'img' => 'dr-diane-savage-pedigo',
+            ],
+            [
+                'name' => 'Dr. Paul L. Nave',
+                'credentials' => 'MD, FAAP',
+                'img' => 'dr-paul-nave',
+            ],
+            [
+                'name' => 'Dr. Ben Spitalnick',
+                'credentials' => 'MD, MBA, FAAP',
+                'img' => 'dr-ben-spitalnick',
+            ],
+            [
+                'name' => 'Dr. Steve Hobby',
+                'credentials' => 'MD, CPC, FAAP',
+                'img' => 'dr-steven-hobby',
+            ],
+            [
+                'name' => 'Dr. Adia H. Wilkes',
+                'credentials' => 'MD, FAAP',
+                'img' => 'dr-adria-wilkes',
+            ],
+            [
+                'name' => 'Dr. Chintak B. Patel',
+                'credentials' => 'MD, FAAP',
+                'img' => 'dr-chintak-patel',
+            ],
+            [
+                'name' => 'Dr. Brandy Gheesling',
+                'credentials' => 'MD, CLC, FAAP',
+                'img' => 'dr-brandy-gheesling',
+            ],
+            [
+                'name' => 'Dr. Giselle M. Rosinia',
+                'credentials' => 'MD, FAAP',
+                'img' => 'dr-giselle-rosinia',
+            ],
+            [
+                'name' => 'Elaine Nussbaum',
+                'credentials' => 'RN, APN',
+                'img' => 'elaine-nussbaum',
+            ],
+        ];
+
+        $data = [
+            'meta' => $meta,
+            'offices' => $offices,
+            'doctors' => $doctors,
+        ];
+
+        return view('home', $data);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,231 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    {{--<link rel="icon" href="/favicon.ico">--}}
+
+    <meta name="description" content="{{ $meta['description'] }}">
+    <meta name="author" content="">
+
+    <title>{{ $meta['title'] }}</title>
+
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    {{--<link rel="stylesheet" href="//localhost:8081/css/styles.css">--}}
+</head>
+
+<body>
+
+<header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col text-center">
+                <img
+                        id="logo"
+                        src="{{ asset('img/logo.png') }}"
+                        alt="Pedatric Associates of Savannah"
+                        srcset="{{ asset('img/logo.svg') }}">
+            </div>
+        </div>
+    </div>
+</header>
+
+<section id="offices">
+    <div class="container">
+        <div class="row">
+
+            @foreach ($offices as $office)
+
+                <div class="col-md-4">
+
+                    <div class="ghost-box">
+                        <h2>{{ $office['title'] }}</h2>
+
+                        <address class="gutter-bottom">
+                            <p class="with-icon before-phone">
+                                <i class="fa fa-phone"></i>
+                                {{ $office['phone'] }}
+                            </p>
+                            <p class="with-icon before-map-marker">
+                                <i class="fa fa-map-marker"></i>
+                                {{ $office['address'] }} <br>
+                                {{ $office['city'] }}, GA {{ $office['zip'] }}
+                            </p>
+                            <p class="with-icon before-clock-o address-box">
+                                <i class="fa fa-phone"></i>
+
+                                @foreach ($office['hours'] as $hours)
+
+                                    {{ $hours }} <br>
+
+                                @endforeach
+                            </p>
+                        </address>
+
+                        <div class="center">
+                            <span class="btn btn-sm btn-secondary">
+                                More Details
+                            </span>
+                        </div>
+                    </div> <!-- // ghost box -->
+
+                </div>
+
+            @endforeach
+
+        </div>
+        
+        <div class="row gutter-top-2x">
+            <div class="col-md-12 center header-buttons">
+
+                <?php
+                    $buttons = [
+                        [
+                            'href' => 'http://www.pedsav.com/index.php?option=com_content&view=article&id=21&Itemid=23',
+                            'title' => 'Online Bill Pay'
+                        ],
+                        [
+                            'href' => 'http://www.pedsav.com/index.php?option=com_content&view=article&id=23&Itemid=25',
+                            'title' => 'New Parent Form'
+                        ],
+                    ];
+                ?>
+
+                @foreach ($buttons as $button)
+                    <a href="{{ $button['href'] }}"
+                       target="_blank"
+                       rel="nofollow"
+                       class="btn btn-lg btn-orange">
+
+                        {{ $button['title'] }}
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="rocket">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>New Website</h2>
+                <h3>Launching Soon!</h3>
+                <p class="gutter-top">
+                    We're working on a brand-new website to go along with our brand-new look! If you don't find what you're looking for on this page, visit our existing website for additional information &amp; resources.
+                </p>
+                <div class="gutter-top">
+                    <a href="#" class="btn btn-lg btn-secondary">
+                        View Old Site
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6 center">
+                <img
+                        src="{{ asset('img/rocket-with-stars.png') }}"
+                        alt="Image of rocket ship representing a new website launching soon"
+                        srcset="{{ asset('img/rocket-with-stars.svg') }}">
+            </div>
+        </div>
+    </div>
+</section>
+
+<div id="clouds">
+    <img
+            src="{{ asset('img/rocket-clouds.png') }}"
+            alt="Image of rocket ship representing a new website launching soon"
+            srcset="{{ asset('img/rocket-clouds.svg') }}">
+</div>
+
+<section id="doctors">
+    <div class="container text-center" id="doc-container">
+        <div class="row justify-content-md-center">
+
+            @foreach ($doctors as $doctor)
+
+                <div class="col-md-3 col-sm-6 gutter-bottom">
+                    <img
+                            src="{{ asset('img/team/' . $doctor['img']) . '.jpg' }}"
+                            alt="{{ $doctor['name'] }}">
+                    <h4>{{ $doctor['name'] }}</h4>
+                    <p class="smaller-text">{{ $doctor['credentials'] }}</p>
+                </div>
+
+            @endforeach
+
+        </div>
+    </div>
+</section>
+
+<section id="video">
+    <div id="ducky">
+        <img
+                src="{{ asset('img/ducky.png') }}"
+                alt="Image of rubber ducky floating on the water"
+                srcset="{{ asset('img/ducky.svg') }}"
+                aria-hidden="true">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <iframe src="https://player.vimeo.com/video/260049335" width="490" height="276" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </div>
+            <div class="col-md-6">
+                <h3>We Love New Parents!</h3>
+                <p>
+                    We understand that it's a big decision when it comes to choosing your child's first pediatrician.
+                </p>
+                <p>
+                    Pediatric Associates of Savannah has been the baby experts since 1971. Caring from the start.
+                </p>
+                <p class="gutter-top">
+                    <a href="//facebook.com/" target="_blank" rel="nofollow">
+                        <i class="fa fa-facebook biggest-text"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div id="fishy">
+        <img
+                src="{{ asset('img/fishy.png') }}"
+                alt="Image of rubber ducky floating on the water"
+                srcset="{{ asset('img/fishy.svg') }}"
+                aria-hidden="true">
+    </div>
+</section>
+
+<footer class="sm-center">
+    {{--<div class="diag-bg">--}}
+        {{--<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"--}}
+             {{--viewBox="0 0 1365.2 100" enable-background="new 0 0 1365.2 100" xml:space="preserve">--}}
+            {{--<polygon fill="#FFFFFF" points="0,100 1366,7.3 1366,100"/>--}}
+        {{--</svg>--}}
+    {{--</div>--}}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 responsive-right">
+                <img
+                        src="{{ asset('img/logo-slogan.png') }}"
+                        alt="Pediatric Associates of Savannah: Caring from the Start"
+                        srcset="{{ asset('img/logo-slogan.svg') }}">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 sm-half-gutter-top">
+                <p>
+                    Need more info?
+                    <a href="//old.pedsav.com">Visit our old site</a>.
+                </p>
+            </div>
+            <div class="col-md-6 responsive-right">
+                <p class="smallest-text small-gutter-top">
+                    &copy; {{ date('Y') }} Pediatric Associates of Savannah
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
