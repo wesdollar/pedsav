c5e8f7ca9f8791af4e8adc79fea5c90e1055ac29
Wes Dollar <wes@wesdollar.com>
2018-03-29 14:29:43 -0400

    adding favicon, updating author

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    {{--<link rel="icon" href="/favicon.ico">--}}
+    <link rel="icon" href="{{ url('/favicon.ico') }}">
 
     <meta name="description" content="{{ $meta['description'] }}">
-    <meta name="author" content="">
+    <meta name="author" content="Wes Dollar, TranterGrey">
 
     <title>{{ $meta['title'] }}</title>
 
